7dbbde801ec6e1754cf9121368caca6612f3cb51
MonikaPikuzinska <57132665+MonikaPikuzinska@users.noreply.github.com>
2025-07-28 19:46:51 +0200

    add NavBar

---
+++ music-cards/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useAuth } from "../../context/AuthContext";
+
+export const NavBar = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <nav className="fixed top-0 w-full">
+      <div className="mx-auto px-4">
+        <div className="flex items-center justify-end h-16">
+          <div className="hidden md:flex items-center">
+            {user ? (
+              <div className="flex items-center space-x-4">
+                {user.user_metadata?.avatar_url && (
+                  <img
+                    src={user.user_metadata.avatar_url}
+                    alt="User Avatar"
+                    className="w-8 h-8 rounded-full object-cover border-indigo-300 border-2"
+                  />
+                )}
+                <button
+                  onClick={signOut}
+                  className="bg-gray-400 text-amber-50 px-3 py-1 rounded mr-5"
+                >
+                  Sign Out
+                </button>
+              </div>
+            ) : null}
+          </div>{" "}
+          <button
+            onClick={() => console.log("inst")}
+            className="bg-indigo-400 text-amber-50 px-3 py-1 rounded"
+          >
+            Instruction
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
--- music-cards/src/routes/RoutesLinks.tsx
+++ music-cards/src/routes/RoutesLinks.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router";
 import Login from "../pages/Login/Login";
 import Game from "../pages/Game/Game";
+import { NavBar } from "../components/NavBar/NavBar";
 
 function RoutesLinks() {
   return (
     <div className="min-h-screen bg-gray-100 transition-opacity duration-700 pt-20">
-      {/* <Navbar /> */}
+      <NavBar />
       <div className="container mx-auto px-4 py-6">
         <Routes>
           <Route path="/" element={<Login />} />
